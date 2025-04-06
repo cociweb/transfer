892829db54236c02d9d355cf7093adcc233435a8
cociweb <cociweb@users.noreply.github.com>
2025-04-06 23:28:42 +0200

    Update webpack.js

--- webpack.js
+++ webpack.js
@@ -1,5 +1,6 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const webpackRules = require('@nextcloud/webpack-vue-config/rules')
+const TerserPlugin = require('terser-webpack-plugin');
 
 // Custom rule for SVGs
 webpackRules.RULE_SVGS = {
@@ -11,6 +12,12 @@ webpackRules.RULE_SVGS = {
 webpackRules.RULE_ASSETS.test = /\.(png|jpe?g|gif|woff2?|eot|ttf)$/
 
 webpackConfig.module.rules = Object.values(webpackRules)
+
+// Add the optimization property
+webpackConfig.optimization = {
+  minimize: true,
+  minimizer: [new TerserPlugin()],
+};
 
 // Logging configuration for debugging
 console.log('Webpack Configuration:', webpackConfig)
